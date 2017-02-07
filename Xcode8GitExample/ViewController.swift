aa892ae11456d6ab1515cd97ec86cb07845b0d76
Sergei Chernavin <Sergei@sergeis-imac.local>
2017-02-07 13:21:37 +0300

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by Sergei Chernavin on 07/02/2017.
+//  Copyright © 2017 Sergei Chernavin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
